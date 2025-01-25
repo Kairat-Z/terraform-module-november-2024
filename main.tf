9555a6d63d67a29b2fac22eca98d6f5351510bbf
EC2 Default User <ec2-user@ip-172-31-15-182.us-east-2.compute.internal>
2025-01-25 02:18:47 +0000

    12

--- main.tf
+++ main.tf
@@ -6,7 +6,14 @@ resource aws_vpc main {
     cidr_block = var.vpc_cidr
 }
 
-resource "aws_subnet" "main" {
+resource aws_subnet main {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.subnet_cidr
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = var.igw_name
+  }
 }
